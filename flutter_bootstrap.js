ad6b80e32ee552d1f5c372c9e7830f474881df79
nabeellc <10nabeellc10@gmail.com>
2025-03-30 13:27:22 +0530

    Keep local version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,10 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
+<<<<<<< HEAD
     serviceWorkerVersion: "2510730587"
+=======
+    serviceWorkerVersion: "2168079171"
+>>>>>>> bc08180 (contact us update)
   }
 });

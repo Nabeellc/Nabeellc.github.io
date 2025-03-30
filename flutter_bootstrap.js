fc3f1638c6b43f8914caee35b927739cb8ee17ea
nabeellc <10nabeellc10@gmail.com>
2025-03-30 16:58:49 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1028404416"
+    serviceWorkerVersion: "1313064474"
   }
 });
